41e33d8c55847e10db417f86ee0c7c5ab4c74616
Shrik <sharonmargad@email.com>
2026-04-01 15:56:22 +0300

    auto: sync changes | 2026-04-01 15:56

---
+++ add_missing_semels.sql
@@ -0,0 +1,7 @@
+-- הוספת סעיפים חסרים: 242498 ו-723087
+INSERT INTO indexes (id, municipality_id, template_id, key_value, account_code, description, connection_name)
+VALUES
+(gen_random_uuid(),'b863b72a-8552-4e4e-87da-e77ae9013ae1','95b37d2d-c9ea-4ef1-9164-ab5ac642b0c7','242498','1844301840','debit','אחזקה במעונות א.ותי'),
+(gen_random_uuid(),'b863b72a-8552-4e4e-87da-e77ae9013ae1','95b37d2d-c9ea-4ef1-9164-ab5ac642b0c7','242498','1344301930','credit','אחזקה במעונות א.ותי'),
+(gen_random_uuid(),'b863b72a-8552-4e4e-87da-e77ae9013ae1','95b37d2d-c9ea-4ef1-9164-ab5ac642b0c7','723087','1846501840','debit','אחזקת נכים בפנימיות'),
+(gen_random_uuid(),'b863b72a-8552-4e4e-87da-e77ae9013ae1','95b37d2d-c9ea-4ef1-9164-ab5ac642b0c7','723087','1346501930','credit','אחזקת נכים בפנימיות');
